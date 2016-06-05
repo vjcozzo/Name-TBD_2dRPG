6783e4295374317441d8dc1e14a5c22b91c11be4
vjcozzo <vjcozzo@comcast.net>
2016-06-04 19:13:50 -0700

    Adding a basic C++ file and map design 0 as initial documents to be shared.

---
+++ executor.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+/*    using namespace std;*/
+    std::cout << "Hello world! Using C++ in Linux :D\n" << std::endl;
+    return 0;
+}
+
